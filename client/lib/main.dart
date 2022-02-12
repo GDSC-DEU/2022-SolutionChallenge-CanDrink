b0fdf0ec9f0e892ac4213e353db86d613e469166
HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>
2022-02-12 16:29:54 +0900

    feat: Implement Image Classification via TFLite
    Signed-off-by: HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,115 +1,188 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tflite/tflite.dart';
 
-void main() {
+List<CameraDescription> cameras = [];
+
+void main() async {
+  _getPermission();
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(const MyApp());
+}
+
+Future<bool> _getPermission() async {
+  await Permission.camera.request();
+  await Permission.microphone.request();
+
+  if (await Permission.camera.isGranted &&
+      await Permission.microphone.isGranted) {
+    return Future.value(true);
+  } else {
+    return Future.value(false);
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData.dark(),
+      home: const CameraScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CameraScreenState createState() => _CameraScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CameraScreenState extends State<CameraScreen> {
+  bool available = true;
+  CameraController? cameraController;
+  CameraImage? cameraImage;
+  List recognitionsList = [];
 
-  void _incrementCounter() {
+  initCamera() async {
+    cameraController = CameraController(cameras[0], ResolutionPreset.veryHigh);
+    cameraController!.initialize().then(
+      (_) {
+        if (!mounted) {
+          return;
+        }
+
+        setState(
+          () {
+            cameraController?.startImageStream(
+              (image) async {
+                if (available) {
+                  available = false;
+                  cameraImage = image;
+                  runModel();
+                  await Future.delayed(const Duration(seconds: 1));
+                  available = true;
+                }
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future loadModel() async {
+    Tflite.close();
+    await Tflite.loadModel(
+        model: "assets/tflite/ssd_mobilenet.tflite",
+        labels: "assets/tflite/ssd_mobilenet.txt");
+  }
+
+  runModel() async {
+    recognitionsList = (await Tflite.detectObjectOnFrame(
+      bytesList: cameraImage!.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
+      imageHeight: cameraImage!.height,
+      imageWidth: cameraImage!.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResultsPerClass: 1,
+      threshold: 0.4,
+    ))!;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      cameraImage;
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+
+    cameraController?.stopImageStream();
+    Tflite.close();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadModel();
+    initCamera();
+  }
+
+  List<Widget> boxRecognizedObjects(Size screen) {
+    double factorX = screen.width;
+    double factorY = screen.height;
+
+    Color colorPick = Colors.red;
+
+    return recognitionsList.map((result) {
+      return Positioned(
+        left: result["rect"]["x"] * factorX,
+        top: result["rect"]["y"] * factorY,
+        width: result["rect"]["w"] * factorX,
+        height: result["rect"]["h"] * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            border: Border.all(color: Colors.pink, width: 2.0),
+          ),
+          child: Text(
+            "${result['detectedClass']} ${(result['confidenceInClass'] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = colorPick,
+              color: Colors.black,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    Size size = MediaQuery.of(context).size;
+    List<Widget> list = [];
+
+    list.add(
+      Positioned(
+        top: 0.0,
+        left: 0.0,
+        width: size.width,
+        height: size.height - 100,
+        child: SizedBox(
+          height: size.height - 100,
+          child: (!cameraController!.value.isInitialized)
+              ? Container()
+              : AspectRatio(
+                  aspectRatio: cameraController!.value.aspectRatio,
+                  child: CameraPreview(cameraController!),
+                ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+    list.addAll(boxRecognizedObjects(size));
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Container(
+          margin: const EdgeInsets.only(top: 50),
+          color: Colors.black,
+          child: Stack(
+            children: list,
+          ),
+        ),
+      ),
     );
   }
 }
