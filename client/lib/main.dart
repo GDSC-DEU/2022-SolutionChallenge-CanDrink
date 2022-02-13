9d67d5d91130a5aba2879a53ad2d8d8da22f14e3
HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>
2022-02-13 14:22:52 +0900

    feat: Close app when permission is not granted
    Signed-off-by: HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,12 +2,15 @@ import 'package:camera/camera.dart';
 import 'package:candrink/screens/camera_screen.dart';
 import 'package:candrink/services/permission_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 List<CameraDescription> cameras = [];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  getPermission();
+  if (!await getPermission()) {
+    SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+  }
   cameras = await availableCameras();
   runApp(const MyApp());
 }
