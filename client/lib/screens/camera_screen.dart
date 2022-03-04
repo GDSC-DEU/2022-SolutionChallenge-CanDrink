8b2c40fefe520c07b842417254684430bf1fe724
HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>
2022-03-04 17:46:34 +0900

    feat: Implement Image Classification
    Signed-off-by: HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>

--- client/lib/screens/camera_screen.dart
+++ client/lib/screens/camera_screen.dart
@@ -1,7 +1,12 @@
 import 'package:camera/camera.dart';
+import 'package:candrink/services/tflite/image_classification/classifier_quant.dart';
 import 'package:candrink/services/tflite_service.dart';
 import 'package:flutter/material.dart';
+import 'package:image/image.dart' as img;
 import 'package:tflite/tflite.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+
+import '../services/tflite/image_classification/classifier.dart';
 
 class CameraScreen extends StatefulWidget {
   List<CameraDescription> cameras;
@@ -16,6 +21,8 @@ class _CameraScreenState extends State<CameraScreen> {
   bool available = true;
   CameraController? cameraController;
   CameraImage? cameraImage;
+  Category? category;
+  late Classifier _classifier;
   List recognitionsList = [];
 
   initCamera() async {
@@ -60,6 +67,54 @@ class _CameraScreenState extends State<CameraScreen> {
       }
     }
     recognitionsList = newRecognitionsList;
+    _predict();
+  }
+
+  var shift = (0xFF << 24);
+  Future<List<int>?> convertYUV420toImageColor(CameraImage image) async {
+    try {
+      final int width = image.width;
+      final int height = image.height;
+      final int uvRowStride = image.planes[1].bytesPerRow;
+      final int? uvPixelStride = image.planes[1].bytesPerPixel;
+
+      // imgLib -> Image package from https://pub.dartlang.org/packages/image
+      var imgOrig = img.Image(width, height); // Create Image buffer
+
+      // Fill image buffer with plane[0] from YUV420_888
+      for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+          final int uvIndex =
+              uvPixelStride! * (x / 2).floor() + uvRowStride * (y / 2).floor();
+          final int index = y * width + x;
+
+          final yp = image.planes[0].bytes[index];
+          final up = image.planes[1].bytes[uvIndex];
+          final vp = image.planes[2].bytes[uvIndex];
+          // Calculate pixel color
+          int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+          int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
+              .round()
+              .clamp(0, 255);
+          int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
+          // color: 0x FF  FF  FF  FF
+          //           A   B   G   R
+          imgOrig.data[index] = shift | (b << 16) | (g << 8) | r;
+        }
+      }
+
+      img.PngEncoder pngEncoder = img.PngEncoder(level: 0, filter: 0);
+      return pngEncoder.encodeImage(imgOrig);
+    } catch (e) {
+      print(">>>>>>>>>>>> ERROR:" + e.toString());
+    }
+    return null;
+  }
+
+  void _predict() async {
+    var a = await convertYUV420toImageColor(cameraImage!);
+    img.Image? imageInput = img.decodeImage(a!);
+    var pred = _classifier.predict(imageInput);
   }
 
   @override
@@ -73,6 +128,8 @@ class _CameraScreenState extends State<CameraScreen> {
   @override
   void initState() {
     super.initState();
+
+    _classifier = ClassifierQuant();
 
     loadModel();
     initCamera();
