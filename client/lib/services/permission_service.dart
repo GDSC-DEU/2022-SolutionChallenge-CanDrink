8a67f7e37eada3a9ee9a5de9dd6624d3b240610c
HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>
2022-06-05 01:53:04 +0900

    feat: Add Bluetooth Permission
    Signed-off-by: HeeRyun, Jin (qwertycvb) <jins4218@gmail.com>

--- client/lib/services/permission_service.dart
+++ client/lib/services/permission_service.dart
@@ -2,6 +2,9 @@ import 'package:permission_handler/permission_handler.dart';
 
 Future<bool> requestPermissions() async {
   final permissions = [
+    await Permission.bluetooth.request(),
+    await Permission.bluetoothConnect.request(),
+    await Permission.bluetoothScan.request(),
     await Permission.camera.request(),
     await Permission.microphone.request(),
   ];
